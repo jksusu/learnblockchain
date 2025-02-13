b7675ba34e247987c89941bb16f44df3970a19fa
mt <mt@gmail.com>
2025-02-13 17:41:21 +0800

    solidity 银行test

---
+++ solidity/bank.sol
@@ -0,0 +1,54 @@
+pragma solidity <0.9.0;
+
+contract Bank {
+    mapping(address => uint) balances;
+    uint256[3] public topAmounts;
+    address[3] public topDepositors;
+
+    address private owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    event Received(address sender, uint amount);
+
+    receive() external payable {
+        emit Received(msg.sender, msg.value);
+        balances[msg.sender] += msg.value;
+        updateTopDepositors(msg.sender, msg.value);
+    }
+
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+
+    function updateTopDepositors(address user, uint256 amount) internal {
+        for (uint256 i = 0; i < 3; i++) {
+            if (amount > topAmounts[i]) {
+                // 移动后面的用户
+                for (uint256 j = 2; j > i; j--) {
+                    topDepositors[j] = topDepositors[j - 1];
+                    topAmounts[j] = topAmounts[j - 1];
+                }
+                // 更新当前用户
+                topDepositors[i] = user;
+                topAmounts[i] = amount;
+                break;
+            }
+        }
+    }
+
+    function getTopDepositors()
+        public
+        view
+        returns (address[3] memory, uint256[3] memory)
+    {
+        return (topDepositors, topAmounts);
+    }
+
+    function withdraw(uint amount, address withdrawAddress) public {
+        require(msg.sender == owner, "Insufficient permissions"); //管理员可以转账出去
+        payable(withdrawAddress).transfer(amount); // 转账到指定地址
+    }
+}
