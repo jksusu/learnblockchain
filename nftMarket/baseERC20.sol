0362e47c6ea1f64a17f6f82f5025078834b02311
mt <mt@gmail.com>
2025-02-18 16:53:33 +0800

    nft market

--- nftMarket/baseERC20.sol
+++ nftMarket/baseERC20.sol
@@ -1,93 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 interface ITokenReceiver {
-    function tokensReceived(address from, uint256 amount) external;
+    function tokensReceived(address from, uint256 amount) external returns (bool);
 }
-contract BaseERC20 {
-    string public name = "BaseERC20"; 
-    string public symbol = "BERC20"; 
-    uint8 public decimals; 
-    uint256 public totalSupply; 
-    mapping (address => uint256) balances; 
-    mapping (address => mapping (address => uint256)) allowances; 
 
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
+contract BaseERC20WithCallBack is ERC20 {
 
-    //检测某个地址的余额是否足够
-    modifier checkBalances(address _address, uint amount) {
-        require(balances[_address] >= amount, "ERC20: transfer amount exceeds balance");
-        _;
+    constructor() ERC20("BaseERC20", "BaseERC20") {
+        _mint(msg.sender, 100000000 * 10 ** 18);
     }
-
-    constructor() {
-        decimals = 18;
-        totalSupply = 100000000 * (10 ** uint256(decimals));
-        balances[msg.sender] = totalSupply;
-    }
-
-    function balanceOf(address _owner) public view returns (uint256 balance) {
-        return balances[_owner];
-    }
-
-    function transfer(address _to, uint256 _value) public checkBalances(msg.sender, _value)  returns (bool success) {
-        //判断余额是否足够
-        balances[msg.sender] = balances[msg.sender] - _value; 
-        balances[_to] = balances[_to] + _value;
-        emit Transfer(msg.sender, _to, _value);
-        return true;
-    }
-
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-        //检查是否存在权限
-        uint amount = allowances[_from][msg.sender];
-        require(amount >= _value, "ERC20: transfer amount exceeds allowance");
-        require(balances[_from] >= _value, "ERC20: transfer amount exceeds balance");
-
-        balances[_from] -= _value; 
-        balances[_to] += _value;
-
-        allowances[_from][msg.sender] -= _value; // 更新被授权者的额度
-
-        emit Transfer(_from, _to, _value); 
-        return true; 
-    }
-
-    function approve(address _spender, uint256 _value) public returns (bool success) {
-        allowances[msg.sender][_spender] = _value;
-        emit Approval(msg.sender, _spender, _value); 
-        return true; 
-    }
-
-    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {   
-        // write your code here   
-        return allowances[_owner][_spender];  
-    }
-
-    function transferWithCallback(address _to, uint256 _value) public returns (bool success) {
-        balances[msg.sender] -= _value;
-        balances[_to] += _value;
-        emit Transfer(msg.sender, _to, _value);
+    
+    function transferWithCallback(address _from, address _to, uint256 amount) public returns (bool) {
+        _transfer(_from, _to, amount);
         if (isContract(_to)) {
-            ITokenReceiver(_to).tokensReceived(msg.sender, _value);
+            (bool success) = ITokenReceiver(_to).tokensReceived(msg.sender, amount);
+            require(success, "Callback failed");
         }
         return true;
     }
 
     function isContract(address account) internal view returns (bool) {
         uint256 size;
-        assembly {
-            size := extcodesize(account)
-        }
+        assembly { size := extcodesize(account) }
         return size > 0;
-    }
-
-    function transferWithCall(address _from, address _to, uint256 _value) public returns (bool success) {
-        balances[_from] -= _value;
-        balances[_to] += _value;
-        emit Transfer(_from, _to, _value);
-        return true;
     }
 }
--- nftMarket/market.sol
+++ nftMarket/market.sol
@@ -12,9 +12,13 @@ contract NFTMarket {
     }
 
     mapping(uint256 => Listing) public listings;
-    mapping(uint256 => address) public nftmap;//nft是谁的
+    mapping(address => mapping(address => uint256)) public balances; //付款记录
+
     BaseERC20 public token;
     IERC721 public nft;
+
+    event TokensReceived(address, uint256);
+
     constructor(BaseERC20 _token, IERC721 _nft) {
         token = _token;
         nft = _nft;
@@ -22,8 +26,10 @@ contract NFTMarket {
 
     // 上架 NFT
     function list(uint256 tokenId, uint256 price) external {
-        //require(nft.ownerOf(tokenId) == msg.sender, "You are not the owner");
-        //require(price > 0, "Price must be greater than 0");
+        //检查上架的nft拥有者是否是当前发起交易的用户
+        require(nft.ownerOf(tokenId) == msg.sender, "not owner");
+        //上架需要nft拥有者把nft转移到交易所
+        nft.transferFrom(msg.sender, address(this), tokenId);
         listings[tokenId] = Listing({seller: msg.sender, price: price});
     }
 
@@ -31,8 +37,19 @@ contract NFTMarket {
     function buyNFT(uint256 tokenId) external {
         Listing memory listing = listings[tokenId];
         require(listing.price > 0, "NFT not listed");
-        token.transferWithCall(msg.sender, listing.seller, listing.price);//转移支持的代币
-        nftmap[tokenId] = msg.sender;//nft转移到 msg.sender
-        delete listings[tokenId];
+        //调用 token 转账函数转账给交易所，交易所转账给卖家 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
+        token.transferWithCallback(msg.sender, address(this), listing.price);
+        token.transfer(listing.seller, listing.price);
+        //需要把 nft 转给买家，需要交易所上架的时候把nft转移到交易所，或者授权给交易所，这里直接转移到付款人的地址上去
+        nft.safeTransferFrom(address(this), msg.sender, tokenId);
+
+        delete listings[tokenId]; //删除上架列表
+    }
+
+    //收到转账后，sender调用此方法用户改变 合约状态
+    function tokensReceived(address from,uint256 amount) external returns (bool) {
+        balances[address(token)][from] += amount;
+        emit TokensReceived(from, amount);
+        return true;
     }
 }
